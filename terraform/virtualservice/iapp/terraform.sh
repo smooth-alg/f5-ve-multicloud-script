1dd8a7c4c016a8f409a5bb1ef142fb4ec47712c0
Alex Applebaum <a.applebaum@f5.com>
2018-07-26 14:22:23 -0700

    debug terraform

--- terraform/virtualservice/iapp/terraform.sh
+++ terraform/virtualservice/iapp/terraform.sh
@@ -2,13 +2,16 @@
 
 export BASE_DIR=`pwd`;
 export DIR=terraform/virtualservice/iapp;
+export TF_LOG=DEBUG;
 
-terraform_command="terraform apply -lock=false -auto-approve"
+terraform_command="terraform apply -lock=false -auto-approve";
 
 cd $BASE_DIR/$DIR;
 terraform init;
 terraform get -update=true;
 
-echo "${terraform_command} $@"
+echo "${terraform_command} $@";
 ${terraform_command} $@
+
+
 

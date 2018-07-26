45b7aef8ebca9c2ef60fe1d15314404a1c9e6cc6
Alex Applebaum <a.applebaum@f5.com>
2018-07-26 13:07:49 -0700

    add vars

--- terraform/virtualservice/iapp/terraform.sh
+++ terraform/virtualservice/iapp/terraform.sh
@@ -3,7 +3,19 @@
 export BASE_DIR=`pwd`;
 export DIR=terraform/virtualservice/iapp;
 
+# echo "1="
+# echo $1
+# echo "2="
+# echo $2
+# echo "3="
+# echo $3
+# echo "4="
+# echo $4
+# echo "5="
+# echo $5
+
 cd $BASE_DIR/$DIR;
 terraform init;
 terraform get -update=true;
-terraform apply
+terraform apply -auto-approve -var $1 -var $2 -var $3 -var $4 -var $5
+

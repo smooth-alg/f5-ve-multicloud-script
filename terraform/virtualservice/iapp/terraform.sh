f23b611af6738f1607fc0b98144df7df43bccade
Alex Applebaum <a.applebaum@f5.com>
2018-07-26 16:17:03 -0700

    small cleanup

--- terraform/virtualservice/iapp/terraform.sh
+++ terraform/virtualservice/iapp/terraform.sh
@@ -1,14 +1,22 @@
 #!/bin/bash -e
 
+# Small bash wrapper around terraform to support 
+# Spinnaker Script Stage which only runs in a single "command" context
+# Also since script runs from git repo, need to store state in remote location
+# https://www.terraform.io/docs/backends/index.html
+# so doesn't get wiped with every pull (see vip.tf )
+
+#export TF_LOG=DEBUG;
 export BASE_DIR=`pwd`;
+# folder in git repo
 export DIR=terraform/virtualservice/iapp;
-export TF_LOG=DEBUG;
-
-terraform_command="terraform apply -lock=false -auto-approve";
-
 cd $BASE_DIR/$DIR;
+
+# Initialize terraform custom F5 provider
 terraform init;
 terraform get -update=true;
+
+terraform_command="terraform apply -lock=false -auto-approve";
 
 echo "${terraform_command} $@";
 ${terraform_command} $@

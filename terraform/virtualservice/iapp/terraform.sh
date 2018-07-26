598b3bf6635c19403ba3ce7cd05047bc5edea6b6
Alex Applebaum <a.applebaum@f5.com>
2018-07-26 13:57:57 -0700

    pass vars

--- terraform/virtualservice/iapp/terraform.sh
+++ terraform/virtualservice/iapp/terraform.sh
@@ -3,19 +3,12 @@
 export BASE_DIR=`pwd`;
 export DIR=terraform/virtualservice/iapp;
 
-# echo "1="
-# echo $1
-# echo "2="
-# echo $2
-# echo "3="
-# echo $3
-# echo "4="
-# echo $4
-# echo "5="
-# echo $5
+terraform_command="terraform apply -lock=false -auto-approve"
 
 cd $BASE_DIR/$DIR;
 terraform init;
 terraform get -update=true;
-terraform apply -auto-approve -var $1 -var $2 -var $3 -var $4 -var $5
+
+echo "${terraform_command} $@"
+${terraform_command} $@
 
